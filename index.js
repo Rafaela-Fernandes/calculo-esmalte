5731b4318b638dd872723190186e139accc8099d
Rafaela Fernandes <rafaela.fernandes@inema.ba.gov.br>
2020-06-03 03:07:31 -0300

    corrigindo calculo

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 
 $(document).ready(function () {
-    $('.qtd').css('color', '#FFF')
+
     $('#bt-01, #bt-02').change(function () {
         let bt_01 = $('#bt-01').val();
         let bt_02 = $('#bt-02').val();
         var totalBitola = 1 / ((parseInt(bt_01) * parseInt(bt_02)) / 10000);
 
-        let texto = $('.qtd').text()
-
-        if (texto != "NaN") {
-            $('.qtd').css('color', '#FFF')
-        } else {
-            $('.qtd').css('color', '#545b62')
-        }
 
         if (bt_01 && bt_02 == 10) {
 
             let total10X10 = parseInt(totalBitola) + 4
-            $('.qtd').text(total10X10.toFixed(2));
-        } else {
+            if (total10X10 >= 0) {
+                $('.total-geral').toggle();
+                $('.qtd').text(total10X10.toFixed(2));
+            }
 
-            $('.qtd').text(totalBitola.toFixed(2));
+        } else {
+            if (totalBitola >= 0) {
+                $('.total-geral').toggle();
+                $('.qtd').text(totalBitola.toFixed(2));
+            }
+
+
         }
     })
 
@@ -40,9 +41,6 @@ $(document).ready(function () {
         let totalGeral = (cal_02 / 1000) * metros;
 
         $('.total-geral').text(totalGeral);
-
-
-
 
     })
 
